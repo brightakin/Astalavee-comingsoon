45c75ff8d9aa4ee8016a76bdd7f43ae3f5b4af0d
brightakin <akinkuotubright@gmail.com>
2022-10-22 16:30:16 -0700

    initial commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,9 @@ import headerImage from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <header className="h-[100px]  sm:w-[100%] bg-slate-50">
+    <header className="h-[100px]  sm:w-[100%] bg-white">
       <img
-        className="absolute h-[40px] w-[300px] left-[20px] sm:left-[80px] top-[38px]"
+        className="absolute h-[30px] sm:h-[40px] w-[200px] sm:w-[300px] left-[20px] sm:left-[80px] top-[38px]"
         src={headerImage}
         alt=""
       />
